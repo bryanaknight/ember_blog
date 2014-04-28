65bd3c62a58482e86d4dbcae1cecdf83757cbd92
Bryana Knight <brknig11@gmail.com>
2014-04-27 20:13:59 -0600

    Post dynamically populating on main page. Each post is a link to redirect to post page.

--- js/app.js
+++ js/app.js
@@ -3,5 +3,24 @@ App = Ember.Application.create();
 App.Router.map(function() {
 	this.resource('about');	
 	this.resource('posts');
+	this.resource('post', { path: ':post_id'});
 });
+
+App.PostsRoute = Ember.Route.extend({
+	model: function () {
+		return posts;
+	}
+});
+
+var posts = [{
+	id: 1,
+	title: "fake post #1",
+	author: "Bryana",
+	body: "Learning Ember!"
+}, {
+	id: 2,
+	title: "fake post #2",
+	author: "Bryana",
+	body: "this is fun!"
+}];
 
